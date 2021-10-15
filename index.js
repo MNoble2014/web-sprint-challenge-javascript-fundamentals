80f083e28a567614394b0cec1c5057c62b677819
MNoble2014 <michealsimpson2013@gmail.com>
2021-10-15 13:05:10 -0400

    Topic 1, Task 2 test passing

--- index.js
+++ index.js
@@ -28,10 +28,14 @@ myFunction();
     
 For example, `summation(4)` should return 10 because 1+2+3+4 is 10. Note, you may use a for loop for this function if you wish */
 
-function summation(/*Your Code Here*/) {
-  /*Your Code Here*/
-
+function summation(num) {
+  let count = 0;
+  for (let i = 1; i <= num; i++) {
+    count += i;
   }
+  return count;
+}
+console.log(summation(4));
  
 
 // 🦁🦁🦁 Topic 2: ADVANCED Array Methods 🦁🦁🦁
@@ -56,10 +60,9 @@ const zooAnimals = [
   displayNames will be an array of strings, and each string should follow this pattern: "name: {name}, scientific: {scientific name}"
   */
 
-  function animalNames(/*Your Code Here*/){
+  function animalNames(/*Your Code Here*/) {
     /*Your Code Here*/
   }
- 
   
 
   /* 🦁🦁🦁 Request 2: .map() 🦁🦁🦁
@@ -81,7 +84,7 @@ const zooAnimals = [
   Using lowPopulationAnimals use .filter() to create a new array of objects which contains only the animals with a population of less than 5.
   */
 
-  function lowPopulationAnimals(/*Your Code Here*/){
+  function lowPopulationAnimals(/*Your Code Here*/) {
     /*Your Code Here*/
   }
   
@@ -92,7 +95,7 @@ const zooAnimals = [
   Remember the reduce method takes two arguments: a callback (which itself takes two args - the accumulator and the item), and an initial value for the count.
   */
 
-  function USApop(/*Your Code Here*/){
+  function USApop(/*Your Code Here*/) {
     /*Your Code Here*/
   }
   
